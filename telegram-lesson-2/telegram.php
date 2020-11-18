eb15d6ce6ee7bc32982824a38a86af86ec6aa2af
AlekseiBurdonov <57301415+AlekseiBurdonov@users.noreply.github.com>
2020-11-18 09:49:27 +0300

    Add files via upload

---
+++ telegram-lesson-2/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot1438888329:AAGhGl-U-mq0PZWgQ9w_ybjNdCQ2AZLdJ-I/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "1438888329:AAGhGl-U-mq0PZWgQ9w_ybjNdCQ2AZLdJ-I";
+$chat_id = "-375696997";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  echo "Спасибо!";
+} else {
+  echo "Error";
+}
+?> */
